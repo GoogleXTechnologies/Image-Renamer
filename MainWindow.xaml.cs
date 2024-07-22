01c4fa9d4031f940a610ba27e702fafb2adf07d2
Lasantha Karunarathne <lasantha.20223087@iit.ac.lk>
2024-07-22 15:05:26 +0530

    updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-//using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
 using System.Security.AccessControl;
